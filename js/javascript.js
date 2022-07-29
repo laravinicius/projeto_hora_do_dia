ca1f5379ce6601758afaa0cb55765216020275ad
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-07-29 13:16:02 -0300

    Update
    update na hora atual

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,36 @@
 function carregar() {
     let msg = document.getElementById(`msg`);
     let img = document.getElementById(`imagem`);
-    var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas!`
+    let data = new Date();
+    let hora = data.getHours();
+
+    // VERSAO QUE EU FIZ PARA MOSTRAR APENAS A HORA DO MOMENTO QUE A TELA É ATUALIZADA
+    // let dia = data.getDate();
+    // let mes = (data.getMonth() + 1);
+    // let ano = data.getFullYear();
+    // let min = data.getMinutes();
+    // msg.innerHTML = `Hoje é dia ${dia}/${mes}/${ano} e agora são ${hora}:${min}`;
+
+
+    // VERSAO MELHORADA PARA PEGAR A HORA ATUAL E EXIBIR NA TELA
+    // funcao para formatar 1 em 01
+    const real = n =>{
+        return (`0` + n).slice(-2);
+    }
+
+    // criando um intevalo de tempo
+    const interval = setInterval(() =>{
+        // pega a hora atual
+        const now = new Date();
+        
+        //formatar para dd/mm/aa hh:mm:ss
+        const dataHora = `${real(now.getUTCDate())} / ${real((now.getMonth() +1 ))} / ${now.getFullYear()}   ${real(now.getHours())} : ${real(now.getMinutes())} : ${real(now.getSeconds())} `
+        msg.innerHTML = dataHora;
+
+    }, 1000);
+    
+
+    
     if(hora >=0 && hora < 12){
         //bomdia
         img.src = '../projeto_hora_do_dia/img/manha.png';
